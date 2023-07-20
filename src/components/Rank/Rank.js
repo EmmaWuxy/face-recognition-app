009cb3cee9da07b7d040b5830711e7c225a47f47
emmaWuxy <emmamiao333@gmail.com>
2023-07-19 23:55:22 -0400

    Rank skeleton

---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,14 @@
+function Rank() {
+    return (
+        <>
+            <div className="white f3">
+                {'Emma, your current rank is...'}
+            </div>
+            <div className="white f1">
+                {'#5'}
+            </div>
+        </>
+    )
+}
+
+export default Rank;
