bbe1883241457462ae504f285d7bba5664dea61b
emmaWuxy <emmamiao333@gmail.com>
2023-07-22 23:49:45 -0400

    Call Clarify API for faces bounding box of image in the response

--- src/App.js
+++ src/App.js
@@ -1,21 +1,82 @@
+import React from 'react';
 import './App.css';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import ParticlesBg from 'particles-bg';
 
-function App() {
-  return (
-    <div className="App">
-      <ParticlesBg color="#FFFFFF" num={200} type="cobweb" bg={true}/>
-      <Navigation />
-      <Logo />
-      <Rank />
-      <ImageLinkForm />
-      {/* <FaceRecognition /> */}
-    </div>
-  );
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+      imageUrl: ''
+    }
+  }
+
+  onInputChange = (event) => {
+    this.setState({input: event.target.value});
+  }
+
+  onButtonSubmit = () => {
+    this.setState({imageUrl: this.state.input});
+    // Your PAT (Personal Access Token) can be found in the portal under Authentification
+    const PAT = 'ba7a7543d94744c28a3c51b1af4c7bf8';
+    // Specify the correct user_id/app_id pairings
+    // Since you're making inferences outside your app's scope
+    const USER_ID = 'emmawxy';
+    const APP_ID = 'face-detection';
+    const MODEL_ID = 'face-detection';
+
+    const raw = JSON.stringify({
+      "user_app_id": {
+        "user_id": USER_ID,
+        "app_id": APP_ID
+      },
+      "inputs": [
+        {
+          "data": {
+            "image": {
+              "url": this.state.input
+            }
+          }
+        }
+      ]
+    });
+
+    const requestOptions = {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Authorization': 'Key ' + PAT
+      },
+      body: raw
+    };
+
+    fetch("https://api.clarifai.com/v2/models/" + MODEL_ID + "/outputs", requestOptions)
+      .then(response => response.json())
+      .then(result => console.log(result.outputs[0].data.regions[0].region_info.bounding_box))
+      .catch(error => console.log('error', error));
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <ParticlesBg color="#FFFFFF" num={200} type="cobweb" bg={true} />
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm
+          onInputChange={this.onInputChange}
+          onButtonSubmit={this.onButtonSubmit}
+        />
+        <FaceRecognition imageUrl={this.state.imageUrl}/>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,14 +1,14 @@
 import './ImageLinkForm.css'
 
-function ImageLinkForm() {
+function ImageLinkForm({ onInputChange, onButtonSubmit }) {
     return (
         <>
             <p>
                 {'This Magic will detect faces in your picture. Enter the link of your chosen picture and hit the button, annd wait for the magic to work.'}
             </p>
             <div className="pa4 br3 shadow-5 form center">
-                <input type="text" className="f4 pa2 w-70"/>
-                <button className="w-30 grow f4 link ph3 pv2 white bg-light-purple center">Detect</button>
+                <input type="text" className="f4 pa2 w-70" onChange={onInputChange}/>
+                <button className="w-30 grow f4 link ph3 pv2 white bg-light-purple center" onClick={onButtonSubmit}>Detect</button>
             </div>
         </>
     )
