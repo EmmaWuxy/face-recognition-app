c206847212d5d254d55691150caf43dcc71faaed
emmaWuxy <emmamiao333@gmail.com>
2023-07-27 00:08:51 -0400

    change form to div

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -2,7 +2,7 @@ function SignIn({ onRouteChange }) {
     return (
         <article className="br4 ba b--black-10 mv4 w-100 w-50-m w-25-l mw5 shadow-5 center">
             <main className="pa4 black-80" >
-                <form className="measure">
+                <div className="measure">
                     <fieldset
                         id="sign_up"
                         className="ba b--transparent ph0 mh0"
@@ -55,7 +55,7 @@ function SignIn({ onRouteChange }) {
                             Register
                         </p>
                     </div>
-                </form>
+                </div>
             </main >
         </article>
     );
